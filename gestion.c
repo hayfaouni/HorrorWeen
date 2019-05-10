dcbbe0f7f4c5f366c0b351936bb5caa9eb2d44de
hayfaouni <48762758+hayfaouni@users.noreply.github.com>
2019-05-10 10:52:49 +0200

    Add files via upload

--- gestion.c
+++ gestion.c
@@ -3,63 +3,21 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include "gestion.h"
-
-void afficherscore(SDL_Surface *ecran,int score)
-{
-
-   SDL_Surface *texte = NULL;
-    SDL_Rect position;
-    SDL_Event event;
-    TTF_Font *police = NULL;
-    SDL_Color couleurNoire = {0, 0, 0};
-    int continuer = 1;
-   char txt[100];
-
-    TTF_Init();
-
-  police = TTF_OpenFont("o.ttf", 65);
-    sprintf(txt, "score : %d ", score);
-
-    texte = TTF_RenderText_Blended(police, txt, couleurNoire);
-
-    while (continuer)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                continuer = 0;
-                break;
-        }
-
-       
-
-        position.x = 300;
-        position.y = 0;
-        SDL_BlitSurface(texte, NULL, ecran, &position); 
-        SDL_Flip(ecran);
-    }
-
-    TTF_CloseFont(police);
-    TTF_Quit();
-
-    SDL_FreeSurface(texte);
-    SDL_Quit();
-
-    //return EXIT_SUCCESS;
-}
-
+/**  * @brief  To initialize vie .  * @param vie the life  * @param url  the url of the image   * @return Nothing  */ 
 void initialiservie(vie *vie)
 {
 vie->nb=3;
 vie->position.x = 0 ;
 vie->position.y = 0 ;
+vie->position2.x = 0 ;
+vie->position2.y = 0 ;
 vie->fond1 = IMG_Load("v1.png");
 vie->fond2 = IMG_Load("v2.png");
 vie->fond3 = IMG_Load("v3.png");
 vie->fond4 = IMG_Load("v4.png");
 vie->fond5 = IMG_Load("GameOver.png");
 }
+/**  * @brief  To free life surface .  * @param vie the life  * @param url  the url of the image   * @return Nothing  */ 
 void freesurfacevie(vie *vie)
 {
 SDL_FreeSurface(vie->fond1);
@@ -68,7 +26,7 @@ SDL_FreeSurface(vie->fond3);
 SDL_FreeSurface(vie->fond4);
 SDL_FreeSurface(vie->fond5);
 }
- 
+ /**  * @brief  To show life   surface .  * @param vie the life  * @param url  the url of the image   * @return Nothing  */ 
 
 void affichervie(vie *vie,perso perso,SDL_Surface *ecran)
 {
--- gestion.h
+++ gestion.h
@@ -1,20 +1,21 @@
 #ifndef GESTION_H_
 #define GESTION_H_
 
-
+/**  * @struct vie  
+* @brief struct for gestion de vie et affichage */ 
 typedef struct vie
 {
 	int nb;
-	SDL_Rect position;
-	SDL_Rect position2;
-	SDL_Surface *fond1;
-	SDL_Surface *fond2;
-	SDL_Surface *fond3;
-	SDL_Surface *fond4;
-	SDL_Surface *fond5;
+	SDL_Rect position; /*!< Rectangle*/
+	SDL_Rect position2; /*!< Rectangle*/
+	SDL_Surface *fond1;/*!< Surface. */ 
+	SDL_Surface *fond2;/*!< Surface. */ 
+	SDL_Surface *fond3;/*!< Surface. */ 
+	SDL_Surface *fond4;/*!< Surface. */ 
+	SDL_Surface *fond5;/*!< Surface. */ 
 }vie;
 
-void afficherscore(SDL_Surface *ecran,int score);
+
 void initialiservie(vie *vie);
 void freesurfacevie(vie *vie);
 void affichervie(vie *vie,perso perso,SDL_Surface *ecran);
